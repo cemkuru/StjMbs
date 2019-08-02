c5761c0b28a67eba3ed70cf5e1092694960757a3
humeyrabenli <humeyrabenli98@gmail.com>
2019-08-02 17:51:30 +0300

    announcements bölümü eklendi ve add,update,delete işlemleri yapıldı

---
+++ Abis.Mbs.MvcWebUI/Controllers/AnnouncementController.cs
@@ -0,0 +1,29 @@
+﻿using Abis.Mbs.Business.Abstract;
+using Abis.Mbs.MvcWebUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Abis.Mbs.MvcWebUI.Controllers
+{
+    public class AnnouncementController : Controller
+    {
+        private IAnnouncementService _announcementService;
+        public AnnouncementController(IAnnouncementService announcementService)
+        {
+            _announcementService = announcementService;
+        }
+        public ActionResult Index(int page = 1)
+        {
+
+            var announcements = _announcementService.GetAll();
+            AnnouncementListViewModel model = new AnnouncementListViewModel
+            {
+                Announcements = announcements
+            };
+            return View(model);
+        }
+    }
+}
